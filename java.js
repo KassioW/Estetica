c02368f089e52974b08ca697e1f08379bcbc55a9
KassioW <86385006+KassioW@users.noreply.github.com>
2023-02-10 06:46:21 -0300

    atualizandotd

--- java.js
+++ java.js
@@ -1,7 +1,14 @@
 document.querySelector(".inicio").addEventListener("click", inicioFunction);
 document.querySelector(".precos").addEventListener("click", precoFunction);
-document.querySelector(".agendar").addEventListener("click", agendarFunction);
 document.querySelector(".servicos").addEventListener("click", servicoFunction);
+document.querySelector(".agendar").addEventListener("click", agendarFunction);
+document.querySelector(".btnagendar").addEventListener("click", btnagendarFunction);
+document.querySelector(".btnagendar1").addEventListener("click", btnagendarFunction);
+document.querySelector(".btnagendar2").addEventListener("click", btnagendarFunction);
+document.querySelector(".btnagendar3").addEventListener("click", btnagendarFunction);
+document.querySelector(".btnagendar4").addEventListener("click", btnagendarFunction);
+document.querySelector(".btnagendar5").addEventListener("click", btnagendarFunction);
+
 
 function inicioFunction(){
   window.location.href = "inicio.html";
@@ -12,6 +19,9 @@ function precoFunction(){
 }
 
 function agendarFunction(){
+  window.location.href = "agendar.html";
+}
+function btnagendarFunction(){
   window.location.href = "agendar.html";
 }
 
