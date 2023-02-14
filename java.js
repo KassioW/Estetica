9aa496b874e5150d20c41fc0a6404c438af872a4
KassioW <86385006+KassioW@users.noreply.github.com>
2023-02-14 03:44:43 -0300

    upando

--- java.js
+++ java.js
@@ -11,6 +11,16 @@ document.querySelector(".btnagendar5").addEventListener("click", btnagendarFunct
 document.querySelector(".btnconsulta").addEventListener("click", btnagendarFunction);
 document.querySelector(".btnconsulta1").addEventListener("click", btnagendarFunction);
 
+function selecionarPagina(span) {
+  // Remove a classe "selecionado" de todas as spans
+  let spans = document.querySelectorAll(".inicio, .precos, .agendar, .servicos");
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].classList.remove("selecionado");
+  }
+
+  // Adiciona a classe "selecionado" à span clicada
+  span.classList.add("selecionado");
+}
 
 function inicioFunction(){
   window.location.href = "inicio.html";
@@ -31,21 +41,32 @@ function servicoFunction(){
   window.location.href = "serviços.html";
 }
 
-// seleciona todos os botões
-const botoes = document.querySelectorAll('span, a');
+function selecionarPagina(elemento) {
+  // remove a classe "active" de todos os elementos de botão
+  document.querySelectorAll(".inicio, .precos, .agendar, .servicos").forEach(function(botao) {
+    botao.classList.remove("active");
+  });
+  
+  // adiciona a classe "active" ao botão selecionado
+  elemento.classList.add("active");
+}
 
-// adiciona um event listener para cada botão
-botoes.forEach(botao => {
-botao.addEventListener('mouseover', () => {
-botao.style.backgroundColor = 'yellow';
+document.addEventListener("DOMContentLoaded", function() {
+  // obter todos os spans
+  var spans = document.querySelectorAll(".inicio, .precos, .agendar, .servicos");
+
+  // encontrar o span mais visível na janela do navegador
+  var mostVisibleSpan;
+  var smallestDistanceToTop = Infinity;
+  for (var i = 0; i < spans.length; i++) {
+    var distanceToTop = Math.abs(spans[i].getBoundingClientRect().top);
+    if (distanceToTop < smallestDistanceToTop) {
+      mostVisibleSpan = spans[i];
+      smallestDistanceToTop = distanceToTop;
+    }
+  }
+
+  // adicionar a classe "active" ao span mais visível
+  mostVisibleSpan.classList.add("active");
 });
-botao.addEventListener('mouseout', () => {
-botao.style.backgroundColor = '';
-});
-botao.addEventListener('click', () => {
-botao.classList.add('animacao');
-setTimeout(() => {
-botao.classList.remove('animacao');
-}, 1000);
-});
-});
+
