585e8dca8a631697cc720e89a3adf69b8284b650
KassioW <86385006+KassioW@users.noreply.github.com>
2023-02-14 03:57:00 -0300

    upp

--- java.js
+++ java.js
@@ -41,32 +41,3 @@ function servicoFunction(){
   window.location.href = "serviços.html";
 }
 
-function selecionarPagina(elemento) {
-  // remove a classe "active" de todos os elementos de botão
-  document.querySelectorAll(".inicio, .precos, .agendar, .servicos").forEach(function(botao) {
-    botao.classList.remove("active");
-  });
-  
-  // adiciona a classe "active" ao botão selecionado
-  elemento.classList.add("active");
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  // obter todos os spans
-  var spans = document.querySelectorAll(".inicio, .precos, .agendar, .servicos");
-
-  // encontrar o span mais visível na janela do navegador
-  var mostVisibleSpan;
-  var smallestDistanceToTop = Infinity;
-  for (var i = 0; i < spans.length; i++) {
-    var distanceToTop = Math.abs(spans[i].getBoundingClientRect().top);
-    if (distanceToTop < smallestDistanceToTop) {
-      mostVisibleSpan = spans[i];
-      smallestDistanceToTop = distanceToTop;
-    }
-  }
-
-  // adicionar a classe "active" ao span mais visível
-  mostVisibleSpan.classList.add("active");
-});
-
